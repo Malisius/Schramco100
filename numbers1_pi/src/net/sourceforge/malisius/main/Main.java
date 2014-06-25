0150aac76f13e96418a61df78bb6c5594780186d
malisius <smiz098@gmail.com>
2014-06-25 01:14:31 -0400

    Finished first program from Numbers.
    Finished numbers1_pi. May be inaccurate to one or two digits.

--- numbers1_pi/src/net/sourceforge/malisius/main/Main.java
+++ numbers1_pi/src/net/sourceforge/malisius/main/Main.java
@@ -1,6 +1,54 @@
 package net.sourceforge.malisius.main;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import javax.swing.JOptionPane;
+
 public class Main 
 {
+	public static void main(String[] args) throws FileNotFoundException, IOException
+	{
+		int n = getDigit();
+		System.out.println("Printing " + n + " digits of pi.");
+		System.out.println(getPi(n));
+	}
+	
+	public static int getDigit()
+	{
+		String input = JOptionPane.showInputDialog(null, "Enter number of digits to display");
+		int n = Integer.parseInt(input);
+		return n;
+	}
+	
+	public static String getPi(int n) throws FileNotFoundException, IOException
+	{
+		String fin = "";
+		
+		try
+		{
+			InputStream in = new FileInputStream(new File("pi.txt"));
+	        Reader reader = new InputStreamReader(in);
 
+	        int c;
+	        for(int x = 0; x < n; x++) 
+	        {
+	        	c = reader.read();
+	            fin = fin.concat(String.valueOf((char) c));
+	        }
+	        
+	        reader.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return fin;
+	}
 }
---
+++ numbers2_fibonacci/src/net/sourceforge/malisius/main/MainUI.java
@@ -0,0 +1,61 @@
+package net.sourceforge.malisius.main;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JSpinner;
+import java.awt.Font;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+
+public class MainUI extends JFrame {
+
+	private JPanel contentPane;
+	private JSpinner spinner;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainUI frame = new MainUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 200, 200);
+		contentPane = new JPanel();
+		contentPane.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		spinner = new JSpinner();
+		spinner.setFont(new Font("Dialog", Font.BOLD, 30));
+		contentPane.add(spinner);
+		
+		JButton btnNewButton = new JButton("Find nth digit");
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Find n in sequence");
+		contentPane.add(btnNewButton_1);
+	}
+
+}
